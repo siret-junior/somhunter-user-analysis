014f66f6705f7c94f37fe8f7d0c663bdf2febe30
skrhakv <vitek.skrhak@seznam.cz>
2021-10-25 19:57:29 +0200

    add data from constrained study

--- add_browsing_session_ids.py
+++ add_browsing_session_ids.py
@@ -4,18 +4,21 @@ import numpy as np
 # script for creating result_collection with browsing ID
 
 if __name__ == "__main__":
-    data = pd.read_csv('result_collection.csv').sort_values(['timestamp'])
-    data.insert(0, "browsing_id", np.nan)
-    data_grouped = data.groupby('user')
-    for username, group in data_grouped:
-        browsing_id_it = -1
-        print(username)
-        for index, row in group.iterrows():
-            if row['type'] == "reset" or row['type'] == 'text':
-                browsing_id_it += 1
-            data.at[index, 'browsing_id'] = browsing_id_it
+    original_names = ['result_collection.csv', 'result_collection_validation.csv']
+    final_names = ['result_collection_browsing_id.csv', 'result_collection_browsing_id_validation.csv']
+    for i in range(len(original_names)):
+        data = pd.read_csv(original_names[i]).sort_values(['timestamp'])
+        data.insert(0, "browsing_id", np.nan)
+        data_grouped = data.groupby('user')
+        for username, group in data_grouped:
+            browsing_id_it = -1
+            print(username)
+            for index, row in group.iterrows():
+                if row['type'] == "reset" or row['type'] == 'text':
+                    browsing_id_it += 1
+                data.at[index, 'browsing_id'] = browsing_id_it
 
-    data.to_csv('result_collection_browsing_id.csv', index=False)
+        data.to_csv(final_names[i], index=False)
 
 
 
